cdcb7f3228e25c5f3ae3583f2f4736b00c0c37b3
Piyush Jasaiwal <coderpiyush123@gmail.com>
2020-09-11 04:44:14 +0530

    second

---
+++ dance_website_node_JS/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const port = 80
+
+//for serving the static files
+app.use('\static',express.static('static'));
+
+
+//pug specific stuff
+app.set('view-engine', 'pug')
+
+app.set('views',path.join(__dirname,'views'))
+
+app.get('/',(req,res)=>{
+    res.status(200).render('home.pug');
+});
+
+app.listen(port,()=>{
+    console.log(`server is started at ${port}`);
+});
